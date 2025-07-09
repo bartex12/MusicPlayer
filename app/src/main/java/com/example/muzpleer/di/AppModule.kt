f3e886a3fc731a7042146c2881b6512dfe9f1cd0
bartex12 <37575336+bartex12@users.noreply.github.com>
2025-07-09 19:56:32 +0300

    работает - теперь выбор - остановка или дальше и загрузка из других мест и шаблон =

--- app/src/main/java/com/example/muzpleer/di/AppModule.kt
+++ app/src/main/java/com/example/muzpleer/di/AppModule.kt
@@ -1,8 +1,6 @@
 package com.example.muzpleer.di
 
 import com.example.muzpleer.model.PlaylistRepository
-import com.example.muzpleer.model.Repository
-import com.example.muzpleer.model.RepositoryImpl
 import com.example.muzpleer.service.MusicServiceHandler
 import com.example.muzpleer.ui.player.PlayerViewModel
 import com.example.muzpleer.ui.tracks.TracksViewModel
@@ -10,7 +8,6 @@ import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
 
 val appModule = module {
-    single<Repository> { RepositoryImpl(get()) }
     single { MusicServiceHandler(get()) }
     single { PlaylistRepository()}
 
