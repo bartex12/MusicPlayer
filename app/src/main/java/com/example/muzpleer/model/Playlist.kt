74bf56ab054c1dbf13b87be5a02c1c58fe9d49a0
bartex12 <37575336+bartex12@users.noreply.github.com>
2025-07-09 19:52:28 +0300

    работает - теперь выбор - остановка или дальше и загрузка из других мест и шаблон

--- app/src/main/java/com/example/muzpleer/model/Playlist.kt
+++
@@ -1,8 +0,0 @@
-package com.example.muzpleer.model
-
-data class Playlist(
-    val id: String,
-    val name: String,
-    val tracks: List<MediaItemApp>,
-    val createdAt: Long = System.currentTimeMillis()
-)
--- app/src/main/java/com/example/muzpleer/ui/player/State.kt
+++
@@ -1,7 +0,0 @@
-package com.example.muzpleer.ui.player
-
-sealed class State (
-   val  IsPlaying:State,
-   val  IsStop:State,
-   val  IsPause:State
-)
