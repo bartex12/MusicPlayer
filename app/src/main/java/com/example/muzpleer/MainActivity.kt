a5b86290341bac6dfb6dadccdcae2b764b7abbc1
bartex12 <37575336+bartex12@users.noreply.github.com>
2025-07-17 23:00:16 +0300

    проблема с разрешениями к файлу на SD - после изменения кода

--- app/src/main/java/com/example/muzpleer/MainActivity.kt
+++ app/src/main/java/com/example/muzpleer/MainActivity.kt
@@ -66,7 +66,6 @@ class MainActivity : AppCompatActivity() {
         scanForMusic()
     }
 
-
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
         // Inflate the menu; this adds items to the action bar if it is present.
         menuInflater.inflate(R.menu.main, menu)
