0c5dc813ea282ea0852cb725ec5851e911c6773d
bartex12 <37575336+bartex12@users.noreply.github.com>
2025-08-09 23:18:05 +0300

    начал плеер внизу

--- app/src/main/java/com/example/muzpleer/ui/local/TabLocalFragment.kt
+++ app/src/main/java/com/example/muzpleer/ui/local/TabLocalFragment.kt
@@ -1,5 +1,6 @@
 package com.example.muzpleer.ui.local
 
+import android.content.ContentUris
 import android.graphics.Color
 import android.os.Bundle
 import android.util.Log
@@ -9,13 +10,17 @@ import android.view.MenuInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
 import androidx.appcompat.widget.SearchView
+import androidx.core.net.toUri
 import androidx.core.view.MenuHost
 import androidx.core.view.MenuProvider
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.Lifecycle
 import androidx.navigation.fragment.findNavController
 import androidx.viewpager.widget.ViewPager
+import com.bumptech.glide.Glide
 import com.example.muzpleer.R
 import com.example.muzpleer.databinding.FragmentTabslocalBinding
 import com.example.muzpleer.model.Song
@@ -24,6 +29,7 @@ import com.example.muzpleer.ui.local.frags.SongFragment
 import com.example.muzpleer.ui.local.viewmodel.SharedViewModel
 import com.google.android.material.tabs.TabLayout
 import org.koin.androidx.viewmodel.ext.android.activityViewModel
+import org.w3c.dom.Text
 import java.util.Locale
 import kotlin.getValue
 
@@ -38,6 +44,13 @@ class TabLocalFragment: Fragment()  {
     private lateinit var tabLayout: TabLayout
     private lateinit var adapter: ViewPageAdapterLocal
     private val viewModel: SharedViewModel by activityViewModel()
+
+    private lateinit var title: TextView
+    private lateinit var artist: TextView
+    private lateinit var artWork: ImageView
+    private lateinit var previous: ImageView
+    private lateinit var playPause: ImageView
+    private lateinit var next: ImageView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -58,7 +71,22 @@ class TabLocalFragment: Fragment()  {
         //устанавливаем текущую вкладку - берём из преференсис     0- вкладка секундомера
         viewPager.currentItem  =  0
         //Log.d(TAG, "***TabsFragment setFragmentResultListener tabPosition = ${viewPager.currentItem}")
-
+        viewModel.currentSong.observe(viewLifecycleOwner) {currentSong->
+            currentSong?. let {
+                title.text=currentSong.title
+                artist.text=currentSong.artist
+                // Загружаем обложку, если есть
+                val albumArtUri=ContentUris.withAppendedId(
+                    "content://media/external/audio/albumart".toUri(),
+                    currentSong.albumId
+                )
+                Glide.with(requireContext())
+                    .load(if (currentSong.isLocal) albumArtUri else it.cover)
+                    .placeholder(R.drawable.muz_player3)
+                    .error(R.drawable.muz_player3)
+                    .into(artWork)
+            }
+        }
     }
 
     override fun onPause() {
@@ -69,6 +97,16 @@ class TabLocalFragment: Fragment()  {
     private fun initViews() {
         viewPager = binding.viewPagerLocal
         tabLayout = binding.tabLayoutLocal
+        title = binding.playerBottom.title
+        artist = binding.playerBottom.artist
+        artWork = binding.playerBottom.artwork
+        previous = binding.playerBottom.previous
+        playPause = binding.playerBottom.playPause
+        next = binding.playerBottom.next
+
+        previous.setOnClickListener {  }
+        playPause.setOnClickListener {  }
+        next.setOnClickListener {  }
     }
 
     private fun initPageAdapter() {
@@ -76,7 +114,7 @@ class TabLocalFragment: Fragment()  {
         viewPager.adapter = adapter
         tabLayout.setupWithViewPager(viewPager)
         //устанавливаем цвет текста черный а при выделении - синий
-        tabLayout.setTabTextColors(Color.BLUE, Color.WHITE)
+        tabLayout.setTabTextColors(Color.GREEN, Color.WHITE)
         tabLayout.setSelectedTabIndicatorColor(Color.WHITE)
     }
 
