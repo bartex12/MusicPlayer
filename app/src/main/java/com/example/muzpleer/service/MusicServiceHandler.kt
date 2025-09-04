ee8530542e4504a18e13caaebf6eb51e8826fa60
bartex12 <37575336+bartex12@users.noreply.github.com>
2025-09-04 13:37:45 +0300

    всё работает кроме загрузки картинки в PlayerFragment

--- app/src/main/java/com/example/muzpleer/service/MusicServiceHandler.kt
+++ app/src/main/java/com/example/muzpleer/service/MusicServiceHandler.kt
@@ -81,7 +81,7 @@ class MusicServiceHandler(
         player?.let { p ->
             val uri = track.getContentUri()
             val mediaItem =MediaItem.fromUri(uri)
-            Log.d(TAG, "@@@MusicServiceHandler playTrack: uri = $uri")
+            Log.d(TAG, "@@@MusicServiceHandler playTrack: uri = $uri artUri = ${track.artUri}")
             p.setMediaItem(mediaItem)
             p.prepare()
             p.play()
