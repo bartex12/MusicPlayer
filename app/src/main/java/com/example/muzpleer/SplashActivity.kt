83120e961f5eaca79214b19c9ca5848c8edd4aec
bartex12 <37575336+bartex12@users.noreply.github.com>
2026-07-26 17:27:27 +0300

    Заставка с анимацией - нужно пе=оменять первую картинку на логотип приложения

--- app/src/main/java/com/example/muzpleer/SplashActivity.kt
+++ app/src/main/java/com/example/muzpleer/SplashActivity.kt
@@ -1,29 +1,48 @@
 package com.example.muzpleer
 
+import android.animation.Animator
 import android.content.Intent
 import android.os.Bundle
+import android.view.animation.LinearInterpolator
 import androidx.appcompat.app.AppCompatActivity
-import androidx.lifecycle.ViewModelProvider
-import com.example.muzpleer.ui.local.viewmodel.SharedViewModel
-import org.koin.android.ext.android.inject
+import com.example.muzpleer.databinding.ActivitySplashBinding
 
 class SplashActivity: AppCompatActivity() {
 
-    val viewModel: SharedViewModel by inject()   // если используете Koin в Activity
+    private lateinit var binding: ActivitySplashBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        // Запускаем загрузку данных
-        loadData()
-    }
+        binding = ActivitySplashBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-    private fun loadData() {
-       viewModel.startSplash{
-           // После загрузки переходим в MainActivity
-           startActivity(Intent(this, MainActivity::class.java))
-           finish()
-       }
+        binding.imageViewSplash.animate()
+            .scaleY(2.5f)
+            .scaleX(2.5f)
+            .setInterpolator(LinearInterpolator()).setDuration(1500)
+            .setListener(object : Animator.AnimatorListener {
 
+                override fun onAnimationStart(p0: Animator) {}
+                override fun onAnimationEnd(p0: Animator) {
+                    startActivity(Intent(this@SplashActivity, MainActivity::class.java))
+                    finish()
+                }
+                override fun onAnimationCancel(p0: Animator) {}
+                override fun onAnimationRepeat(p0: Animator) {}
+            })
+
+//        window.apply {
+//            clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS)
+//            addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
+//            statusBarColor = android.graphics.Color.TRANSPARENT
+//            decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN or
+//                    View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+//        }
+//
+//        // Просто ждём 2 секунды и переходим, а сканирование запустится в MainActivity
+//        Handler(Looper.getMainLooper()).postDelayed({
+//            startActivity(Intent(this, MainActivity::class.java))
+//            finish()
+//        }, 1000)
     }
 }
